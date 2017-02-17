8a91a1689337fb114ec76b8b64862c788efa69b8
yangzc <yangzc@knowbox.cn>
2017-02-17 12:15:49 +0800

    code sorting

--- shells/moveaar.sh
+++ shells/moveaar.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 if [ "$1" = "student" ]
 then
-	echo "student"
+	rm -rf ~/devsoft/projects/android/AndroidRCStudent/base-release/library-release.aar
+	rm -rf ~/devsoft/projects/android/AndroidRCStudent/coretext/coretext.aar
+	cp ~/devsoft/projects/android/AndroidKnowboxBase/library/build/outputs/aar/library-release.aar ~/devsoft/projects/android/AndroidRCStudent/base-release/library-release.aar
+	cp ~/devsoft/projects/android/AndroidKnowboxBase/coretext/coretext.aar ~/devsoft/projects/android/AndroidRCStudent/coretext/coretext.aar
+	echo "student copy done"
 else
 	rm -rf ~/devsoft/projects/android/AndroidRCTeacher/boxbase/library-release.aar
 	rm -rf ~/devsoft/projects/android/AndroidRCTeacher/coretext/coretext.aar
 	cp ~/devsoft/projects/android/AndroidKnowboxBase/library/build/outputs/aar/library-release.aar ~/devsoft/projects/android/AndroidRCTeacher/boxbase/library-release.aar
 	cp ~/devsoft/projects/android/AndroidKnowboxBase/coretext/coretext.aar ~/devsoft/projects/android/AndroidRCTeacher/coretext/coretext.aar
-	echo "teacher done"
+	echo "teacher copy done"
 fi
