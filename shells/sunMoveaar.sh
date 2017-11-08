ad259884cc3c8f00794c0b7e4abeb70702daab33
sunyangyang <sunyangyang@yang.local>
2017-11-08 14:28:56 +0800

    En:添加sh文件

---
+++ shells/sunMoveaar.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+./gradlew library:assembleRelease
+if [ "$1" = "student" ]
+then
+	rm -rf ~/AndroidRCStudent/base-release/library-release.aar
+	cp ~/AndroidKnowboxBase/library/build/outputs/aar/library-release.aar ~/AndroidRCStudent/base-release/library-release.aar
+	echo "student copy done"
+else
+	rm -rf ~/AndroidRCTeacher/boxbase/library-release.aar
+	cp ~/AndroidKnowboxBase/library/build/outputs/aar/library-release.aar ~/AndroidRCTeacher/library-release/library-release.aar
+	echo "teacher copy done"
+fi
