6df5a95793ac48377e0f2bf67a3d681c4abf1559
sun0816 <sunyy@knowbox.cn>
2018-03-12 10:13:28 +0800

    En:添加resume和pause，并在audioblock中pause添加暂停功能

--- shells/sunMoveaar.sh
+++ shells/sunMoveaar.sh
@@ -2,9 +2,15 @@
 ./gradlew library:assembleRelease
 #if [ "$1" = "student" ]
 #then
-#	rm -rf ~/AndroidRCStudent/base-release/library-release.aar
-#	cp ~/AndroidKnowboxBase/library/build/outputs/aar/library-release.aar ~/AndroidRCStudent/libs/library-release.aar
-#	echo "student copy done"
+	rm -rf ~/AndroidRCStudent/libs/library-release.aar
+	cp ~/AndroidKnowboxBase/library/build/outputs/aar/library-release-0.1.aar ~/AndroidRCStudent/libs/library-release-0.1.aar
+
+	rm -rf ~/AndroidRCStudent/dotread/libs/library-release.aar
+	cp ~/AndroidKnowboxBase/library/build/outputs/aar/library-release-0.1.aar ~/AndroidRCStudent/dotread/libs/library-release-0.1.aar
+
+	rm -rf ~/AndroidRCStudent/commons/libs/library-release.aar
+	cp ~/AndroidKnowboxBase/library/build/outputs/aar/library-release-0.1.aar ~/AndroidRCStudent/commons/libs/library-release-0.1.aar
+	echo "student copy done"
 #else
 #	rm -rf ~/AndroidRCTeacher/boxbase/library-release.aar
 #	cp ~/AndroidKnowboxBase/library/build/outputs/aar/library-release.aar ~/AndroidRCTeacher/library-release/library-release.aar
